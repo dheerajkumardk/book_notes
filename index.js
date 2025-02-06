e04f3bc8703f1a4e6f1733832c7bf35eeffa8e8f
Dheeraj <dyaduwanshi0@gmail.com>
2025-02-06 16:50:21 +0530

    add routes to edit book contents in the db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import pg from "pg";
-import { render } from "ejs";
+import methodOverride from "method-override";
 
 const app = express();
 const port = 3000;
@@ -18,6 +18,8 @@ const db = new pg.Client({
 db.connect();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 let books = [];
 
@@ -71,7 +73,33 @@ app.get("/books/:id", async (req, res) => {
 });
 
 // GET    /books/:id/edit -> render a form to edit informations for the book
+app.get("/books/:id/edit", async (req, res) => {
+    const id = req.params.id;
+    let book;
+    try {
+        const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+        book = result.rows[0];
+    } catch (err) {
+        console.log(err);
+    }
+    res.render("edit.ejs", {
+        book: book
+    });
+});
+
 // PUT    /books/:id      -> update information about the book
+app.put("/books/:id/edit", async (req, res) => {
+    const id = req.params.id;
+    const {date_read, rating, notes} = req.body;
+
+    try {
+        const result = await db.query("UPDATE books SET date_read = $1, rating = $2, notes = $3 WHERE id = $4", [date_read, rating, notes, id]);
+        res.redirect("/books");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 // DELETE /books/:id      -> delete the entry from the database
 
 app.listen(port, () => {
